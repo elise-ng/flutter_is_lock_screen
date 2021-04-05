6178f9fa12098d9c2ee65280d6efcecb42a9a506
Elliot Ng <chihimng.io@gmail.com>
2021-04-06 04:39:29 +0800

    feat: migrate plugin to support null safety

--- lib/is_lock_screen.dart
+++ lib/is_lock_screen.dart
@@ -4,9 +4,9 @@ import 'package:flutter/services.dart';
 
 final _channel = MethodChannel('is_lock_screen');
 
-Future<bool> isLockScreen() async {
+Future<bool?> isLockScreen() async {
   try {
-    return await _channel.invokeMethod('isLockScreen') as bool;
+    return await _channel.invokeMethod('isLockScreen') as bool?;
   } catch (e) {
     print(e.toString());
     return null;
