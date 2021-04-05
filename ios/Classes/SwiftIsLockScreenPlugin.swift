05e307f40f3f6c85577d6d9da174804c35fa9042
Elliot Ng <chihimng.io@gmail.com>
2021-04-06 04:59:53 +0800

    chore: remove unused code and debug print

--- ios/Classes/SwiftIsLockScreenPlugin.swift
+++ ios/Classes/SwiftIsLockScreenPlugin.swift
@@ -11,16 +11,6 @@ public class SwiftIsLockScreenPlugin: NSObject, FlutterPlugin {
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch (call.method) {
         case "isLockScreen":
-// Does not work
-//            DispatchQueue.main.async {
-//                // ref: https://stackoverflow.com/a/46002893
-//                let prev = UIScreen.main.brightness
-//                UIScreen.main.brightness = prev + (prev <= 0.01 ? 0.01 : -0.01)
-//                let new = UIScreen.main.brightness
-//                print("1prev \(prev) new \(new)")
-//                return result(prev != new)
-//            }
-            print(UIScreen.main.brightness)
             return result(UIScreen.main.brightness == 0.0)
         default:
             return result(FlutterMethodNotImplemented)
